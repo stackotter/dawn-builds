f6c9e478f7069d5f6d382632af61334d3725eee6
stackotter <stackotter@stackotter.dev>
2022-03-12 11:27:19 +1000

    Copy dawn.json and automatically generate build.zip

--- build_macos.sh
+++ build_macos.sh
@@ -9,7 +9,7 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 cp scripts/standalone.gclient .gclient
 PATH="$PWD/depot_tools:$PATH" gclient sync
 
-# Setup for building
+# Setup build configurations
 mkdir -p out/Release_x64
 mkdir -p out/Release_arm64
 
@@ -21,9 +21,45 @@ echo "is_debug=false
 use_system_xcode=true
 target_cpu=\"arm64\"" > out/Release_arm64/args.gn
 
+# Generate ninja project
 PATH="$PWD/depot_tools:$PATH" gn gen out/Release_x64
 PATH="$PWD/depot_tools:$PATH" gn gen out/Release_arm64
 
 # Build
 PATH="$PWD/depot_tools:$PATH" ninja -C out/Release_x64/ src/dawn/native:shared src/dawn:proc_shared
 PATH="$PWD/depot_tools:$PATH" ninja -C out/Release_arm64/ src/dawn/native:shared src/dawn:proc_shared
+
+cd ..
+
+# Copy headers
+rm -rf build
+mkdir -p build/include
+cp -R dawn/include/* build/include
+cp -R dawn/out/Release_x64/gen/include/* build/include # Assumes headers are the same for both architectures
+
+# Copy binaries
+mkdir -p build/lib/x64
+mkdir -p build/lib/arm64
+cp dawn/out/Release_x64/*.dylib build/lib/x64
+cp dawn/out/Release_arm64/*.dylib build/lib/arm64
+
+# Merge binaries into universal binaries
+cd build/lib
+lipo -create -output libdawn_native.dylib x64/libdawn_native.dylib arm64/libdawn_native.dylib
+lipo -create -output libdawn_proc.dylib x64/libdawn_proc.dylib arm64/libdawn_proc.dylib
+lipo -create -output libEGL.dylib x64/libEGL.dylib arm64/libEGL.dylib
+lipo -create -output libGLESv2.dylib x64/libGLESv2.dylib arm64/libGLESv2.dylib
+lipo -create -output libvk_swiftshader.dylib x64/libvk_swiftshader.dylib arm64/libvk_swiftshader.dylib
+lipo -create -output libVkICD_mock_icd.dylib x64/libVkICD_mock_icd.dylib arm64/libVkICD_mock_icd.dylib
+lipo -create -output libVkLayer_khronos_validation.dylib x64/libVkLayer_khronos_validation.dylib arm64/libVkLayer_khronos_validation.dylib
+cd ../..
+
+# Copy dawn.json
+cp dawn/dawn.json build/lib
+
+# Clean up build directory
+rm -rf build/lib/arm64
+rm -rf build/lib/x64
+
+# Zip build
+zip -r build.zip build
